c181413d3cc648177a6490a54ea6a3438402d60f
Jonny Arnold <jonny.arnold@reevoo.com>
2015-11-12 11:45:16 +0000

    REPL continues if it cannot parse the expression

--- repl.rb
+++ repl.rb
@@ -10,13 +10,31 @@ end
 
 runtime = Block.new
 
+# Keep asking for lines!
 loop do
-  print '> '
+  print 'fn> '
   line = gets.chomp
 
-  begin
-    puts execute(runtime, line)
-  rescue FnRunError => e
-    puts e
+  # Try to execute the line;
+  # if it doesn't work, assume the user wants to continue.
+  # FIXME: this makes syntax errors look like continuation requests...
+  loop do
+    runtime_sandbox = runtime.clone
+
+    begin
+      puts execute(runtime_sandbox, line)
+
+      # Successful execution; set the runtime to the sandbox.
+      runtime = runtime_sandbox
+      break
+    rescue FnRunError => e
+      # It ran, but ended in a Fn failure.
+      puts e
+      break
+    rescue StandardError => e
+      # The Parser errored. Keep asking!
+      print '... '
+      line += gets.chomp
+    end
   end
 end
